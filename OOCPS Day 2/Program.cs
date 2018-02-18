62ea055d0ac0afd0f29318ecf02b105ac2917ffe
weinchester <e0282927@u.nus.edu>
2018-02-18 15:19:11 +0800

    Add project files.

---
+++ OOCPS Day 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOCPS_Day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
