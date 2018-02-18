6a242c7328bbda5ff8445bd52fd2b0a1665cb6a6
weinchester <e0282927@u.nus.edu>
2018-02-18 16:34:46 +0800

    Day 2

---
+++ OOCPS Day 2/AliasTester.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOCPS_Day_2
+{
+    class AliasTester
+    {
+        public static void Main()
+        {
+            MyNum objA = new MyNum(5);
+            MyNum objB = new MyNum(8);
+            Console.WriteLine("Original Values:");
+            Console.WriteLine("Object A is " + objA.GetValue());
+            Console.WriteLine("Object B is " + objB.GetValue());
+
+            objA.SetValue(666);
+            Console.WriteLine("\nObject A is changed before Assignment");
+            Console.WriteLine("Object A is " + objA.GetValue());
+            Console.WriteLine("Object B is " + objB.GetValue());
+
+            objA = objB;
+            Console.WriteLine("\nAfter Assignment");
+            Console.WriteLine("Object A is " + objA.GetValue());
+            Console.WriteLine("Object B is " + objB.GetValue());
+
+            objA.SetValue(444);
+            Console.WriteLine("\nObject A is after Assignment");
+            Console.WriteLine("Object A is " + objA.GetValue());
+            Console.WriteLine("Object B is " + objB.GetValue()); 
+        }
+    }
+}
--- OOCPS Day 2/Program.cs
+++ OOCPS Day 2/Program.cs
@@ -6,10 +6,69 @@ using System.Threading.Tasks;
 
 namespace OOCPS_Day_2
 {
-    class Program
+    public class MyNum
     {
-        static void Main(string[] args)
+        private int val;
+     
+
+        public MyNum(int iniVal)
         {
+            val = iniVal;
+            
         }
+
+        public void SetValue(int newVal )
+        {
+            val = newVal;
+           
+        }
+
+        public int GetValue()
+        {
+            return val;
+        }
+
+
+    }
+    public class Paramtester
+    {
+        public static void Main(string[] args)
+        {
+            int val1 = 111;
+            MyNum val2 = new MyNum(222);
+
+            Console.WriteLine("Before calling change values method");
+            Console.WriteLine("val1 \t val2");
+            Console.WriteLine(val1 + "\t" + val2.GetValue());
+
+            Console.WriteLine();
+
+            ChangeValues(val1, val2);
+
+            Console.WriteLine();
+            Console.WriteLine("After calling change values method");
+            Console.WriteLine("val1 \t val2");
+            Console.WriteLine(val1 + "\t" + val2.GetValue());
+
+        }
+
+        public static void ChangeValues(int val1, MyNum val2)
+        {
+            Console.WriteLine("In ChangeValues() : before changing");
+            Console.WriteLine("val1 \t val2");
+
+            Console.WriteLine(val1 + "\t" + val2.GetValue());
+
+            //changing the values,
+            val1 = 777;
+            val2.SetValue(888);
+
+            Console.WriteLine("In ChangeValues() : after changing");
+            Console.WriteLine("val1 \t val2");
+
+            Console.WriteLine(val1 + "\t" + val2.GetValue());
+
+        }
+
     }
 }
